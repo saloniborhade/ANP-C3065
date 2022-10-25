ea164f8d1d54cbb1fb6e4ddd0935888e332b08fd
Abc <Abc@DESKTOP-CSGNKBC>
2022-10-25 18:49:28 +0530

    version2 New Class Switch Class Demo

---
+++ MyJavaProject1/src/SwitchClassDemo.java
@@ -0,0 +1,33 @@
+
+	public class SwitchClassDemo {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int day=4;
+			switch(day) {
+				case 1:
+					System.out.println("Monday");
+					break;
+				case 2:
+					System.out.println("Tueday");
+					break;
+				case 3:
+					System.out.println("Wedday");
+					break;
+				case 4:
+					System.out.println("Thuday");
+					break;
+				case 5:
+					System.out.println("Friday");
+					break;
+				case 6:
+					System.out.println("Saturday");
+					break;
+				case 7:
+				
+	System.out.println("Sunday");
+					break;
+					}		
+			}
+	}
+	
